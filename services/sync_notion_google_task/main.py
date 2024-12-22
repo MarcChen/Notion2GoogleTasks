11d464ff6ec39bc1d493ee5e4b854a29621dc40c
Marc Chen - Tour <128506536+MarcChen@users.noreply.github.com>
2024-12-22 14:33:55 +0100

    cleaned code

--- services/sync_notion_google_task/main.py
+++ services/sync_notion_google_task/main.py
@@ -161,17 +161,3 @@ class NotionToGoogleTaskSyncer:
             due_date = today
 
         return due_date
-
-if __name__ == "__main__":
-    notion_api_key = os.getenv("NOTION_API")
-    database_id = os.getenv("DATABASE_ID")
-    token_path = os.getenv("TOKEN_PATH")
-    project_root = os.getenv("PROJECT_ROOT")
-
-    assert notion_api_key, "NOTION_API environment variable is required."
-    assert database_id, "DATABASE_ID environment variable is required."
-    assert token_path, "TOKEN_PATH environment variable is required."
-    assert project_root, "PROJECT_ROOT environment variable is required."
-
-    syncer = NotionToGoogleTaskSyncer(notion_api_key, database_id, project_root, token_path)
-    syncer.sync_pages_to_google_tasks()
